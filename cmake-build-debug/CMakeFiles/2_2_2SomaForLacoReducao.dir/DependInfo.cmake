3777c8d53b367849d6e3be33211688bb4590995c
Roberto Hoo <robertohoo@gmail.com>
2021-03-13 23:59:51 -0300

    ok

---
+++ cmake-build-debug/CMakeFiles/2_2_2SomaForLacoReducao.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "F:/Programacao/CParalelaUfrgs2020_2/2.2.2SomaForLacoReducao/SomaForLacoReducao.cpp" "F:/Programacao/CParalelaUfrgs2020_2/2.2.2SomaForLacoReducao/cmake-build-debug/CMakeFiles/2_2_2SomaForLacoReducao.dir/SomaForLacoReducao.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/2_2_2SomaForLacoReducao.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2_2_2SomaForLacoReducao.exe"
+  "2_2_2SomaForLacoReducao.exe.manifest"
+  "2_2_2SomaForLacoReducao.pdb"
+  "CMakeFiles/2_2_2SomaForLacoReducao.dir/SomaForLacoReducao.cpp.obj"
+  "lib2_2_2SomaForLacoReducao.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2_2_2SomaForLacoReducao.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
